252b032ecabd108f6242df67065a9c3139fd7581
Alexs Ruiz <xiopito2@gmail.com>
2021-10-03 10:23:09 -0600

    llamada a la api y configuracion basica del store

---
+++ src/components/ListCharacters.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="characters">
+      <div class="characters__item" v-for="character in characters" :key="character.id">
+          <CardCharacter :character="character"/>
+      </div>
+  </section>
+</template>
+
+<script>
+import { computed, onMounted } from 'vue'
+import {useStore} from 'vuex'
+
+import CardCharacter from './CardCharacter.vue'
+
+export default {
+    components: {
+        CardCharacter
+    },
+    setup() {
+        const store = useStore()
+        const characters = computed(() => {
+            return store.state.charactersFilter
+        })
+
+        onMounted(() => {
+            store.dispatch('getCharacters')
+        })
+
+        return {
+            characters
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
